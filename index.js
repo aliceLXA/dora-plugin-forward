d1c7811010de41efb509f43f8062e78c0a708616
nanyuantingfeng <nanyuantingfeng@163.com>
2017-05-24 01:52:41 +0800

    修正格式化

--- index.js
+++ index.js
@@ -1,71 +1,72 @@
 /***********************************************************
  * AUTHOR : nanyuantingfeng DATE : 4/29/16 TIME : 21:10
  **********************************************************/
-const httpProxy = require('http-proxy');
-const path = require('path');
-const url = require('url');
-const proxy = httpProxy.createProxyServer({ ws : true });
+const httpProxy = require('http-proxy')
+const path = require('path')
+const url = require('url')
+const proxy = httpProxy.createProxyServer({ws: true})
 
 proxy.on('error', e => {
-  console.warn(e);
-});
+  console.warn(e)
+})
 
-function middleware() {
-  let pKg = require(path.resolve(this.cwd, "package.json"));
-  let config = pKg['dora-forward'] || pKg['forward'] || pKg['proxy'];
+function middleware () {
+  let pKg = require(path.resolve(this.cwd, 'package.json'))
+
+  let config = pKg['dora-forward'] || pKg['forward'] || pKg['proxy']
 
   if (!config) {
-    return function *(next) {
+    return function * (next) {
       yield next
     }
   }
 
-  this.log.info(config);
+  //this.log.info(config)
+  this.log.info(JSON.stringify(config, null, 2))
 
-  let gOptions = {};
+  let gOptions = {}
 
   if (config.options) {
-    gOptions = config.options;
-    config.options = undefined;
-    delete config.options;
+    gOptions = config.options
+    config.options = undefined
+    delete config.options
   }
 
-  let rules = config;
+  let rules = config
 
   if (config.rules) {
-    rules = config.rules;
+    rules = config.rules
   }
 
-  let ruleKeys = Object.keys(rules);
+  let ruleKeys = Object.keys(rules)
 
-  return function *(next) {
+  return function * (next) {
+    let i = -1
 
-    for (let i = 0; i < ruleKeys.length; i++) {
+    while (++i < ruleKeys.length) {
       let key = ruleKeys[i]
-      let rule = rules[key];
+      let rule = rules[key]
 
-      if (typeof rule === "string") {
-        rule = { target : rule };
+      if (typeof rule === 'string') {
+        rule = {target: rule}
       }
 
-      let urlObj = url.parse(this.req.url);
+      let urlObj = url.parse(this.req.url)
 
       if (urlObj.pathname.startsWith(key)) {
         return yield (callback) => {
-          this.respond = false;
-          proxy.web(
-            this.req,
-            this.res,
-            Object.assign({ changeOrigin : true }, gOptions, rule, callback)
-          )
+          this.respond = false // koa api
+          let opts = Object.assign({changeOrigin: true}, gOptions, rule, callback)
+          proxy.web(this.req, this.res, opts)
         }
       }
     }
-    yield next;
-  };
+
+    yield next
+  }
 }
 
 module.exports = {
-  name : 'forward3',
-  middleware : middleware,
+  name: 'forward3',
+  middleware: middleware,
 }
